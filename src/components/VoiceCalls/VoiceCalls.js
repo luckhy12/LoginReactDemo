35d3bb01d6c6c46399c242bdff047a729b579213
lucky <lakhwinder.lucky06@gmail.com>
2021-02-24 00:07:54 +0530

    ui updated

--- src/components/VoiceCalls/VoiceCalls.js
+++ src/components/VoiceCalls/VoiceCalls.js
@@ -9,10 +9,13 @@ import Grid from "@material-ui/core/Grid";
 import Fab from "@material-ui/core/Fab";
 import PhoneInput from "react-phone-input-2";
 import MicOffIcon from "@material-ui/icons/MicOff";
-import DialpadIcon from '@material-ui/icons/Dialpad';
-import CallEndIcon from '@material-ui/icons/CallEnd';
-import RecordVoiceOverIcon from '@material-ui/icons/RecordVoiceOver';
-import PhonePausedIcon from '@material-ui/icons/PhonePaused';
+import DialpadIcon from "@material-ui/icons/Dialpad";
+import CallEndIcon from "@material-ui/icons/CallEnd";
+import RecordVoiceOverIcon from "@material-ui/icons/RecordVoiceOver";
+import PhonePausedIcon from "@material-ui/icons/PhonePaused";
+import IconButton from "@material-ui/core/IconButton";
+import BackspaceIcon from "@material-ui/icons/Backspace";
+import HighlightOffIcon from "@material-ui/icons/HighlightOff";
 
 const styles = (theme) => ({
   paper: {
@@ -26,6 +29,10 @@ const styles = (theme) => ({
   },
   flex: {
     flexGrow: 1,
+  },
+  margin: {
+    margin: theme.spacing(1),
+    backgroundColor: "#3f51b5",
   },
 });
 
@@ -104,104 +111,132 @@ class VoiceCalls extends React.Component {
     return (
       <Container component="main">
         <div className="mb-5">
-          <Grid container className={classes.root} justify="center" spacing={2}>
-            <Grid item xs={12} md={6}>
-              <Grid xs={6} container justify="center" spacing={2}>
-                <Grid item xs={4}>
-                  <PhoneInput
-                    country={"us"}
-                    value={this.state.phone}
-                    onChange={(phone) => this.setState({ phone })}
-                  />
+          <Grid container className={classes.flex} justify="center" spacing={2}>
+            <Grid item xs={12} md={12}>
+              <Grid container justify="center">
+                <Grid xs={4} key={"key-1"} justify="center" item>
+                  <div className="col-md-6 ml-3">
+                    <PhoneInput
+                      country={"us"}
+                      value={this.state.phone}
+                      onChange={(phone) => this.setState({ phone })}
+                    />
+                  </div>
                 </Grid>
-                <Grid container justify="center">
-                  <Grid xs={4} key={"key-1"} item>
-                    <Fab color="primary" aria-label="edit">
-                      {1}
-                    </Fab>
-                  </Grid>
-                  <Grid xs={4} key={"key-2"} item>
-                    <Fab color="primary" aria-label="edit">
-                      {2}
-                    </Fab>
-                  </Grid>
-                  <Grid xs={4} key={"key-3"} item>
-                    <Fab color="primary" aria-label="edit">
-                      {3}
-                    </Fab>
-                  </Grid>
-                  <Grid xs={4} key={"key-4"} item>
-                    <Fab color="primary" aria-label="edit">
-                      {4}
-                    </Fab>
-                  </Grid>
-                  <Grid xs={4} key={"key-5"} item>
-                    <Fab color="primary" aria-label="edit">
-                      {5}
-                    </Fab>
-                  </Grid>
-                  <Grid xs={4} key={"key-6"} item>
-                    <Fab color="primary" aria-label="edit">
-                      {6}
-                    </Fab>
-                  </Grid>
-                  <Grid xs={4} key={"key-7"} item>
-                    <Fab color="primary" aria-label="edit">
-                      {7}
-                    </Fab>
-                  </Grid>
-                  <Grid xs={4} key={"key-8"} item>
-                    <Fab color="primary" aria-label="edit">
-                      {8}
-                    </Fab>
-                  </Grid>
-                  <Grid xs={4} key={"key-9"} item>
-                    <Fab color="primary" aria-label="edit">
-                      {9}
-                    </Fab>
-                  </Grid>
-                  <Grid xs={4} key={"key-0"} item>
-                    <Fab color="primary" aria-label="edit">
-                      {0}
-                    </Fab>
-                  </Grid>
-                  <Grid xs={4} key={"back"} item>
-                    <Fab color="primary" aria-label="edit">
-                      {"<-"}
-                    </Fab>
-                  </Grid>
-                  <Grid xs={4} key={"cls"} item>
-                    <Fab color="primary" aria-label="edit">
-                      {"cls"}
-                    </Fab>
-                  </Grid>
+              </Grid>
+            </Grid>
+            <Grid item xs={12} md={3}>
+              <Grid container justify="center">
+                <Grid xs={4} key={"key-1"} item>
+                  <IconButton aria-label="delete" className={classes.margin}>
+                    {1}
+                  </IconButton>
                 </Grid>
-                <Grid container justify="center">
-                  <Grid xs={4} key={"cls"} item>
-                    <Fab color="secondary" aria-label="edit">
-                      <MicOffIcon />
-                    </Fab>
-                  </Grid>
-                  <Grid xs={4} key={"cls"} item>
-                    <Fab color="secondary" aria-label="edit">
-                      <DialpadIcon />
-                    </Fab>
-                  </Grid>
-                  <Grid xs={4} key={"cls"} item>
-                    <Fab color="secondary" aria-label="edit">
-                      <CallEndIcon />
-                    </Fab>
-                  </Grid>
-                  <Grid xs={4} key={"cls"} item>
-                    <Fab color="secondary" aria-label="edit">
-                      <RecordVoiceOverIcon />
-                    </Fab>
-                  </Grid>
-                  <Grid xs={4} key={"cls"} item>
-                    <Fab color="secondary" aria-label="edit">
-                      <PhonePausedIcon />
-                    </Fab>
-                  </Grid>
+                <Grid xs={4} key={"key-2"} item>
+                  <IconButton aria-label="delete" className={classes.margin}>
+                    {2}
+                  </IconButton>
+                </Grid>
+                <Grid xs={4} key={"key-3"} item>
+                  <IconButton aria-label="delete" className={classes.margin}>
+                    {3}
+                  </IconButton>
+                </Grid>
+                <Grid xs={4} key={"key-4"} item>
+                  <IconButton aria-label="delete" className={classes.margin}>
+                    {4}
+                  </IconButton>
+                </Grid>
+                <Grid xs={4} key={"key-5"} item>
+                  <IconButton aria-label="delete" className={classes.margin}>
+                    {5}
+                  </IconButton>
+                </Grid>
+                <Grid xs={4} key={"key-6"} item>
+                  <IconButton aria-label="delete" className={classes.margin}>
+                    {6}
+                  </IconButton>
+                </Grid>
+                <Grid xs={4} key={"key-7"} item>
+                  <IconButton aria-label="delete" className={classes.margin}>
+                    {7}
+                  </IconButton>
+                </Grid>
+                <Grid xs={4} key={"key-8"} item>
+                  <IconButton aria-label="delete" className={classes.margin}>
+                    {8}
+                  </IconButton>
+                </Grid>
+                <Grid xs={4} key={"key-9"} item>
+                  <IconButton aria-label="delete" className={classes.margin}>
+                    {9}
+                  </IconButton>
+                </Grid>
+                <Grid xs={4} key={"key-0"} item>
+                  <IconButton aria-label="delete" className={classes.margin}>
+                    {0}
+                  </IconButton>
+                </Grid>
+                <Grid xs={4} key={"back"} item>
+                  <Fab color="primary" aria-label="edit">
+                    <IconButton aria-label="delete" className={classes.margin}>
+                      <BackspaceIcon />
+                    </IconButton>
+                  </Fab>
+                </Grid>
+                <Grid xs={4} key={"cls"} item>
+                  <Fab color="primary" aria-label="edit">
+                    <HighlightOffIcon />
+                  </Fab>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item xs={12} md={12}>
+              <Grid container justify="center">
+                <Grid xs={2} key={"mic"} item>
+                  <IconButton
+                    aria-label="delete"
+                    color="secondary"
+                    className={classes.margin}
+                  >
+                    <MicOffIcon />
+                  </IconButton>
+                </Grid>
+                <Grid xs={2} key={"dial"} item>
+                  <IconButton
+                    aria-label="delete"
+                    color="secondary"
+                    className={classes.margin}
+                  >
+                    <DialpadIcon />
+                  </IconButton>
+                </Grid>
+                <Grid xs={2} key={"call"} item>
+                  <IconButton
+                    aria-label="delete"
+                    color="secondary"
+                    className={classes.margin}
+                  >
+                    <CallEndIcon />
+                  </IconButton>
+                </Grid>
+                <Grid xs={2} key={"record"} item>
+                  <IconButton
+                    aria-label="delete"
+                    color="secondary"
+                    className={classes.margin}
+                  >
+                    <RecordVoiceOverIcon />
+                  </IconButton>
+                </Grid>
+                <Grid xs={2} key={"pause"} item>
+                  <IconButton
+                    aria-label="delete"
+                    color="secondary"
+                    className={classes.margin}
+                  >
+                    <PhonePausedIcon />
+                  </IconButton>
                 </Grid>
               </Grid>
             </Grid>
